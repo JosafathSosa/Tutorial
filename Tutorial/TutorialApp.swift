162f3a9a6d59f6b82ad7c0da1e792105a26c9f4b
Ramiro Josafath Sosa Jimenez <rsp_63@hotmail.com>
2024-03-05 16:47:39 -0600

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Josafath Sosa Jimenez on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
